71c6b08e42a2a5b1368e4658a4924ba7e78df1dc
Ygor Carvalho <ygcarvalh@gmail.com>
2023-08-09 16:54:44 -0300

    Bump version: 0.1.0 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'erpbrasil.bank.inter'
 year = '2020'
 author = 'Luis Felipe Mileo'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.0'
+version = release = '1.0.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
